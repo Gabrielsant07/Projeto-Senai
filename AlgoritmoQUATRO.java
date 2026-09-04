ea9e00db3fcb2f92c08544cd978073b96c18ac4f
Gabrielsant700 <gabrielsant700@gmail.com>
2026-09-04 09:19:09 -0300

    minha alteracao

---
+++ AlgoritmoQUATRO.java
@@ -0,0 +1,33 @@
+public class AlgoritmoQUATRO {
+    void main(){
+        //Qual é os dias da semana atualmente?
+        int dias = Integer.parseInt(IO.readln("Digite os dias atuais (1 a 7):"));
+        IO.println("Os dias atuais da semana é" + dias);
+        switch (dias) {
+            case 1:
+                IO.println("segunda-feira");
+                break;
+            case 2:
+                IO.println("terça-feira");
+                break;
+            case 3:
+                IO.println("quarta-feira");
+                break;
+            case 4:
+                IO.println("quinta-feira");
+                break;
+            case 5:
+                IO.println("sexta-feira");
+                break;
+            case 6:
+                IO.println("sabado");
+                break;
+            case 7:
+                IO.println("domingo");
+                break;                  
+            default:
+                IO.println("dia invalido");
+                break;
+        }
+    } 
+}
