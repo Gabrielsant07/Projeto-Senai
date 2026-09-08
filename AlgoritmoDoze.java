3a048944b50894cf729e8392e775d93e6127ec7d
Gabrielsant700 <gabrielsant700@gmail.com>
2026-09-08 11:14:03 -0300

    Adicionando novos algoritmos

---
+++ AlgoritmoDoze.java
@@ -0,0 +1,14 @@
+public class AlgoritmoDoze {
+  
+    public static void main(){
+
+        int numero = Integer.parseInt(IO.readln("Digite um número: "));
+
+        if(numero % 2 == 0){
+            IO.println("O número é par");
+        }
+        else{
+            IO.println("O número é ímpar");
+        }
+    }
+}
---
+++ AlgoritmoONZE.java
@@ -0,0 +1,15 @@
+public class AlgoritmoONZE {
+    public static void main(){
+        String nome = IO.readln("digite seu nome;");
+        
+        if("gabriel".equals(nome)){
+            IO.print("olá, seja bem vindo");
+        }
+        else {
+             IO.println("se o nome não for gabriel, não sera bem vindo ");
+        }
+
+            IO.println("fim do programa");
+    }
+}
+   
